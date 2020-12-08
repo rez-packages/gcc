20189482a5a8ade613d8ec471abb002946ada1f8
rfletchr <rcsfletcher@protonmail.com>
2020-12-08 00:25:02 +0000

    update package py

--- package.py
+++ package.py
@@ -1,11 +1,19 @@
 name = "gcc"
+
 version = "4.8.3"
 
 requires=["gmp-4.3.2+", "mpfr-3.1.0+", "mpc-1.0.1+"]
 
+variants=[["platform-linux", "arch-x86_64"]]
+
+tools = ["gcc", "g++", "gfortran"]
+
 def commands():
     env.PATH.append("{root}/bin")
-    env.LD_LIBRARY_PATH.append("{root}/lib")
     env.LD_LIBRARY_PATH.append("{root}/lib64")
+    env.GCC_CXX_INCLUDE_DIR="{root}/include/c++{version}"
+    if building:
+	env.CC="{root}/bin/gcc"
+	env.CXX="{root}/bin/g++"
 
 
